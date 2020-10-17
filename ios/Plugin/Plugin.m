4f6414e2e4f16fd387e77b4e73e8f3be94b92bff
Matt Jewett <mkjewett@yahoo.com>
2020-10-17 11:17:31 -0700

    Restructured Plugin Added getLocation

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -4,8 +4,9 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(PJAMMGeolocation, "PJAMMGeolocation",
-           CAP_PLUGIN_METHOD(startLocation, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(stopLocation, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getLocation, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(startLocation, CAPPluginReturnNone);
+           CAP_PLUGIN_METHOD(stopLocation, CAPPluginReturnNone);
            CAP_PLUGIN_METHOD(enableBackgroundTracking, CAPPluginReturnNone);
            CAP_PLUGIN_METHOD(disableBackgroundTracking, CAPPluginReturnNone);
 )
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -6,12 +6,29 @@ import CoreLocation
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
  */
+
 @objc(PJAMMGeolocation)
 public class PJAMMGeolocation: CAPPlugin, CLLocationManagerDelegate {
 
-    private var locationManager:CLLocationManager?
-
-    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    @objc private var locationManager:CLLocationManager?
+    @objc private var taskKey:NSInteger = 0
+    @objc private var locationCall:CAPPluginCall?
+    
+    @objc public override init!(bridge: CAPBridge!, pluginId: String!, pluginName: String!) {
+        super.init(bridge: bridge, pluginId: pluginId, pluginName: pluginName);
+        
+        DispatchQueue.main.async {
+            self.locationManager = CLLocationManager()
+            self.locationManager?.delegate = self
+            self.locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+            self.locationManager?.activityType = CLActivityType.fitness
+            self.locationManager?.showsBackgroundLocationIndicator = true
+            self.locationManager?.pausesLocationUpdatesAutomatically = false
+            self.locationManager?.allowsBackgroundLocationUpdates = false
+        }
+    }
+    
+    @objc public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
 
         if let location = locations.last {
 
@@ -31,34 +48,37 @@ public class PJAMMGeolocation: CAPPlugin, CLLocationManagerDelegate {
             ]
 
             self.notifyListeners("pjammLocation",data: position)
-
+            locationCall?.resolve(position)
         }
-
+    }
+    
+/**
+ * Capacitor Plugin Methods
+ */
+    
+    @objc func getLocation(_ call: CAPPluginCall) {
+        self.locationCall = call
+        self.locationManager?.requestWhenInUseAuthorization()
+        self.locationManager?.requestAlwaysAuthorization()
+        self.locationManager?.requestLocation()
     }
 
     @objc func startLocation(_ call: CAPPluginCall) {
-
-        DispatchQueue.main.async {
-            self.locationManager = CLLocationManager()
-            self.locationManager?.requestAlwaysAuthorization()
-            self.locationManager?.startUpdatingLocation()
-            self.locationManager?.delegate = self
-        }
-
-        call.resolve()
+        self.locationManager?.requestWhenInUseAuthorization()
+        self.locationManager?.requestAlwaysAuthorization()
+        self.locationManager?.startUpdatingLocation()
     }
 
     @objc func stopLocation(_ call: CAPPluginCall) {
-        locationManager?.stopUpdatingLocation();
-        call.resolve()
+        locationManager?.stopUpdatingLocation()
     }
 
     @objc func enableBackgroundTracking(_ call: CAPPluginCall) {
-        locationManager?.allowsBackgroundLocationUpdates = true
+        self.locationManager?.allowsBackgroundLocationUpdates = true
     }
 
     @objc func disableBackgroundTracking(_ call: CAPPluginCall) {
-        locationManager?.allowsBackgroundLocationUpdates = false
+        self.locationManager?.allowsBackgroundLocationUpdates = false
     }
 
 }
